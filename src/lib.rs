464bf3e8092e70a6bc6c84ecce6c7218961014f1
cc <cc@154.com>
2024-06-02 11:50:56 +0800

    添加`alloc_simple_allocator`过程宏

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,19 @@
-#![no_std]
-
-extern crate proc_macro;
 extern crate buddy;
-
+extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemStatic};
+use syn::{parse_macro_input, Expr, ItemStatic};
 
 #[proc_macro_attribute]
-pub fn define_allocator(attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn define_simple_allocator(attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemStatic);
-    let size = attr.to_string().parse::<usize>().unwrap();
+    let size = parse_macro_input!(attr as Expr);
     let name = &input.ident;
 
     let expanded = quote! {
         #[global_allocator]
-        static #name: simple::SimpleAllocator =simple::SimpleAllocator::new(unsafe { &mut #name });
+        static ALLOC: simple::SimpleAllocator = simple::SimpleAllocator::new(unsafe { &mut #name });
 
         static mut #name: [u8; #size] = [0; #size];
     };
